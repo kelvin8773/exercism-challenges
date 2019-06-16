0577f8c3b9628f117a45b39e45f2778e94d0180f
Kelvin Liang <kelvin9877@gmail.com>
2019-06-16 09:12:21 +0800

    finish highscore

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,18 @@
+class HighScores
+  attr_reader :scores
+  def initialize(scores)
+    @scores = scores
+  end
+  
+  def latest
+    scores.pop
+  end
+
+  def personal_best
+    scores.max
+  end
+
+  def personal_top_three
+    scores.max(3)
+  end
+end
