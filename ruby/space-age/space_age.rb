45f9687f9c33fdee7a1ecc0ed7f1199be4725469
Kelvin Liang <kelvin9877@gmail.com>
2019-07-02 20:17:29 +0800

    update space age

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -9,27 +9,27 @@ class SpaceAge
   end
 
   def on_mercury
-    (self.on_earth / 0.2408467).round(2)
+    on_earth / 0.2408467
   end
 
   def on_venus
-    (self.on_earth / 0.61519726).round(2)
+    on_earth / 0.61519726
   end
 
   def on_mars
-    (self.on_earth / 1.8808158).round(2)
+    on_earth / 1.8808158
   end
 
   def on_jupiter
-    (self.on_earth / 11.862615).round(2)
+    on_earth / 11.862615
   end
 
   def on_saturn
-    (self.on_earth / 29.447498).round(2)
+    on_earth / 29.447498
   end
 
   def on_uranus
-    (self.on_earth / 84.016846).round(2)
+    on_earth / 84.016846
   end
 
   def on_neptune
